3ef4129daed4f75548997dca29f00b20a741289c
akshithareddyk <akshithareddykandhala@gmail.com>
2026-02-17 09:55:01 +0530

    adding concurrency layer class

---
+++ src/main/java/com/zeta/concurrency/ChefWorker.java
@@ -0,0 +1,32 @@
+package com.zeta.concurrency;
+
+import com.zeta.model.ORDER_STATUS;
+import com.zeta.model.Order;
+import com.zeta.service.KitchenService;
+import com.zeta.service.OrderService;
+
+public class ChefWorker implements Runnable {
+    private final OrderService orderService;
+    private final KitchenService kitchenService;
+
+    public ChefWorker(OrderService orderService, KitchenService kitchenService) {
+        this.orderService = orderService;
+        this.kitchenService = kitchenService;
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            try {
+                Order order = kitchenService.takeOrder();
+                System.out.println("Chef {" + Thread.currentThread().getName() + "}" + " started preparing order: " + order.getId());
+                Thread.sleep(order.getTotalPreparationTime() * 1000L);
+                orderService.updateStatus(order.getId(), ORDER_STATUS.PREPARED);
+                System.out.println("Order completed: " + order.getId());
+            } catch(InterruptedException e) {
+                break;
+            }
+        }
+    }
+}
+ 
