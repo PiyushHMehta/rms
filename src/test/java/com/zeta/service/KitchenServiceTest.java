2eb11ca5953920d0fe5d9dcf411d69c217de9ea6
akshithareddyk <akshithareddykandhala@gmail.com>
2026-02-17 12:15:06 +0530

    adding testing code for menuservice and menudao

---
+++ src/test/java/com/zeta/service/KitchenServiceTest.java
@@ -0,0 +1,31 @@
+package com.zeta.service;
+
+import com.zeta.model.Order;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class KitchenServiceTest {
+
+    @Test
+    void testOrdersFollowFIFO() throws InterruptedException {
+        KitchenService kitchenService = new KitchenService();
+        Order order1 = new Order(1, 100,new ArrayList<>());
+        kitchenService.submitOrder(order1);
+        assertEquals(1, kitchenService.takeOrder().getId());
+    }
+
+    @Test
+    void testSubmitOrderAddsToQueue() throws InterruptedException {
+        KitchenService kitchenService = new KitchenService();
+        Order order = new Order(1, 100,new ArrayList<>());
+        kitchenService.submitOrder(order);
+        Order takenOrder = kitchenService.takeOrder();
+        assertNotNull(takenOrder);
+        assertEquals(1, takenOrder.getId());
+        assertEquals(100, takenOrder.getTotalCost());
+    }
+}
